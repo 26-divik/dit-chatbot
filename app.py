347229cd1a560cca2b004877c4ddbb7681cf96fe
Divik Goel <divikgoel75@gmail.com>
2024-07-31 10:27:17 +0530

    new final

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from g4f.client import Client
 from flask import Flask, render_template, request, jsonify, redirect, url_for, session
-
 client = Client()
 
 app = Flask(__name__)
@@ -42,4 +41,4 @@ def chat():
     return jsonify({"response": bot_response})
 
 if __name__ == "__main__":
-   app.run(debug=True, host='0.0.0.0', port=42069)
+   app.run()
